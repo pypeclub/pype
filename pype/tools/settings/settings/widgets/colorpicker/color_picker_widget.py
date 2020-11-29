74203488a2c8bae18b1ae065af3e14452ffeae58
iLLiCiTiT <jakub.trllo@gmail.com>
2020-11-30 00:09:32 +0100

    added cmyk and hsl

--- pype/tools/settings/settings/widgets/colorpicker/color_picker_widget.py
+++ pype/tools/settings/settings/widgets/colorpicker/color_picker_widget.py
@@ -40,6 +40,8 @@ class RGBInputs(QtWidgets.QWidget):
     def __init__(self, color, parent=None):
         super().__init__(parent)
 
+        self._block_changes = False
+
         self.color = color
 
         self.label = "RGB:"
@@ -71,14 +73,20 @@ class RGBInputs(QtWidgets.QWidget):
         self.input_blue = input_blue
 
     def _on_red_change(self, value):
+        if self._block_changes:
+            return
         self.color.setRed(value)
         self._on_change()
 
     def _on_green_change(self, value):
+        if self._block_changes:
+            return
         self.color.setGreen(value)
         self._on_change()
 
     def _on_blue_change(self, value):
+        if self._block_changes:
+            return
         self.color.setBlue(value)
         self._on_change()
 
@@ -86,22 +94,103 @@ class RGBInputs(QtWidgets.QWidget):
         self.value_changed.emit()
 
     def color_changed(self):
+        if (
+            self.input_red.value() == self.color.red()
+            and self.input_green.value() == self.color.green()
+            and self.input_blue.value() == self.color.blue()
+        ):
+            return
+
+        self._block_changes = True
+
         self.input_red.setValue(self.color.red())
         self.input_green.setValue(self.color.green())
         self.input_blue.setValue(self.color.blue())
+
+        self._block_changes = False
 
 
 class CMYKInputs(QtWidgets.QWidget):
     value_changed = QtCore.pyqtSignal()
 
-    def __init__(self, parent=None):
+    def __init__(self, color, parent=None):
         super().__init__(parent)
 
+        self.color = color
+
+        self._block_changes = False
+
+        self.label = "CMYK:"
+
+        input_cyan = QtWidgets.QSpinBox(self)
+        input_magenta = QtWidgets.QSpinBox(self)
+        input_yellow = QtWidgets.QSpinBox(self)
+        input_black = QtWidgets.QSpinBox(self)
+
+        input_cyan.setMinimum(0)
+        input_magenta.setMinimum(0)
+        input_yellow.setMinimum(0)
+        input_black.setMinimum(0)
+
+        input_cyan.setMaximum(255)
+        input_magenta.setMaximum(255)
+        input_yellow.setMaximum(255)
+        input_black.setMaximum(255)
+
+        layout = QtWidgets.QHBoxLayout(self)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(input_cyan)
+        layout.addWidget(input_magenta)
+        layout.addWidget(input_yellow)
+        layout.addWidget(input_black)
+
+        input_cyan.valueChanged.connect(self._on_change)
+        input_magenta.valueChanged.connect(self._on_change)
+        input_yellow.valueChanged.connect(self._on_change)
+        input_black.valueChanged.connect(self._on_change)
+
+        self.input_cyan = input_cyan
+        self.input_magenta = input_magenta
+        self.input_yellow = input_yellow
+        self.input_black = input_black
+
     def _on_change(self):
-        pass
+        if self._block_changes:
+            return
+        self.color.setCmyk(
+            self.input_cyan.value(),
+            self.input_magenta.value(),
+            self.input_yellow.value(),
+            self.input_black.value()
+        )
+        self.value_changed.emit()
 
     def color_changed(self):
-        pass
+        if self._block_changes:
+            return
+        _cur_color = QtGui.QColor()
+        _cur_color.setCmyk(
+            self.input_cyan.value(),
+            self.input_magenta.value(),
+            self.input_yellow.value(),
+            self.input_black.value()
+        )
+        if (
+            _cur_color.red() == self.color.red()
+            and _cur_color.green() == self.color.green()
+            and _cur_color.blue() == self.color.blue()
+        ):
+            return
+
+        c, m, y, k, _ = self.color.getCmyk()
+        self._block_changes = True
+
+        self.input_cyan.setValue(c)
+        self.input_magenta.setValue(m)
+        self.input_yellow.setValue(y)
+        self.input_black.setValue(k)
+
+        self._block_changes = False
 
 
 class HEXInputs(QtWidgets.QWidget):
@@ -125,6 +214,8 @@ class HEXInputs(QtWidgets.QWidget):
         self.input_field = input_field
 
     def _on_change(self):
+        if self._block_changes:
+            return
         input_value = self.input_field.text()
         # TODO what if does not match?
         if self.hex_regex.match(input_value):
@@ -142,32 +233,159 @@ class HEXInputs(QtWidgets.QWidget):
                 and _cur_color.blue() == self.color.blue()
             ):
                 return
+        self._block_changes = True
+
         self.input_field.setText(self.color.name())
+
+        self._block_changes = False
 
 
 class HSVInputs(QtWidgets.QWidget):
     value_changed = QtCore.pyqtSignal()
 
-    def __init__(self, parent=None):
+    def __init__(self, color, parent=None):
         super().__init__(parent)
 
+        self._block_changes = False
+
+        self.color = color
+
+        self.label = "HSV:"
+
+        input_red = QtWidgets.QSpinBox(self)
+        input_green = QtWidgets.QSpinBox(self)
+        input_blue = QtWidgets.QSpinBox(self)
+
+        input_red.setMinimum(0)
+        input_green.setMinimum(0)
+        input_blue.setMinimum(0)
+
+        input_red.setMaximum(255)
+        input_green.setMaximum(255)
+        input_blue.setMaximum(255)
+
+        layout = QtWidgets.QHBoxLayout(self)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(input_red)
+        layout.addWidget(input_green)
+        layout.addWidget(input_blue)
+
+        input_red.valueChanged.connect(self._on_red_change)
+        input_green.valueChanged.connect(self._on_green_change)
+        input_blue.valueChanged.connect(self._on_blue_change)
+
+        self.input_red = input_red
+        self.input_green = input_green
+        self.input_blue = input_blue
+
+    def _on_red_change(self, value):
+        if self._block_changes:
+            return
+        self.color.setRed(value)
+        self._on_change()
+
+    def _on_green_change(self, value):
+        if self._block_changes:
+            return
+        self.color.setGreen(value)
+        self._on_change()
+
+    def _on_blue_change(self, value):
+        if self._block_changes:
+            return
+        self.color.setBlue(value)
+        self._on_change()
+
     def _on_change(self):
-        pass
+        self.value_changed.emit()
 
     def color_changed(self):
-        pass
+        if (
+            self.input_red.value() == self.color.red()
+            and self.input_green.value() == self.color.green()
+            and self.input_blue.value() == self.color.blue()
+        ):
+            return
+
+        self._block_changes = True
+
+        self.input_red.setValue(self.color.red())
+        self.input_green.setValue(self.color.green())
+        self.input_blue.setValue(self.color.blue())
+
+        self._block_changes = False
+
 
 class HSLInputs(QtWidgets.QWidget):
     value_changed = QtCore.pyqtSignal()
 
-    def __init__(self, parent=None):
+    def __init__(self, color, parent=None):
         super().__init__(parent)
 
+        self._block_changes = False
+
+        self.color = color
+
+        self.label = "HSL:"
+
+        input_hue = QtWidgets.QSpinBox(self)
+        input_sat = QtWidgets.QSpinBox(self)
+        input_light = QtWidgets.QSpinBox(self)
+
+        input_hue.setMinimum(0)
+        input_sat.setMinimum(0)
+        input_light.setMinimum(0)
+
+        input_hue.setMaximum(359)
+        input_sat.setMaximum(255)
+        input_light.setMaximum(255)
+
+        layout = QtWidgets.QHBoxLayout(self)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(input_hue)
+        layout.addWidget(input_sat)
+        layout.addWidget(input_light)
+
+        input_hue.valueChanged.connect(self._on_change)
+        input_sat.valueChanged.connect(self._on_change)
+        input_light.valueChanged.connect(self._on_change)
+
+        self.input_hue = input_hue
+        self.input_sat = input_sat
+        self.input_light = input_light
+
     def _on_change(self):
-        pass
+        if self._block_changes:
+            return
+        self.color.setHsl(
+            self.input_hue.value(),
+            self.input_sat.value(),
+            self.input_light.value()
+        )
+        self.value_changed.emit()
 
     def color_changed(self):
-        pass
+        _cur_color = QtGui.QColor()
+        _cur_color.setHsl(
+            self.input_hue.value(),
+            self.input_sat.value(),
+            self.input_light.value()
+        )
+        if (
+            _cur_color.red() == self.color.red()
+            and _cur_color.green() == self.color.green()
+            and _cur_color.blue() == self.color.blue()
+        ):
+            return
+
+        self._block_changes = True
+        h, s, l, _ = self.color.getHsl()
+
+        self.input_hue.setValue(h)
+        self.input_sat.setValue(s)
+        self.input_light.setValue(l)
+
+        self._block_changes = False
 
 
 class ColorInputsWidget(QtWidgets.QWidget):
@@ -180,10 +398,15 @@ class ColorInputsWidget(QtWidgets.QWidget):
 
         hex_input = HEXInputs(color, self)
         rgb_input = RGBInputs(color, self)
+        cmyk_input = CMYKInputs(color, self)
+        hsl_input = HSLInputs(color, self)
+
 
         input_fields = [
             hex_input,
-            rgb_input
+            rgb_input,
+            cmyk_input,
+            hsl_input
         ]
 
         inputs_widget = QtWidgets.QWidget(self)
@@ -203,11 +426,14 @@ class ColorInputsWidget(QtWidgets.QWidget):
 
         hex_input.value_changed.connect(self._on_value_change)
         rgb_input.value_changed.connect(self._on_value_change)
+        cmyk_input.value_changed.connect(self._on_value_change)
 
         self.input_fields = input_fields
 
         self.hex_input = hex_input
         self.rgb_input = rgb_input
+        self.cmyk_input = cmyk_input
+        self.hsl_input = hsl_input
 
         self.color = color
 
