78eaa450e760ee519bec0108c5d97bf2a50b951a
iLLiCiTiT <jakub.trllo@gmail.com>
2020-11-29 23:32:18 +0100

    added few input fields for color

--- pype/tools/settings/settings/widgets/colorpicker/color_picker_widget.py
+++ pype/tools/settings/settings/widgets/colorpicker/color_picker_widget.py
@@ -1,4 +1,5 @@
 import sys
+import re
 from Qt import QtWidgets, QtCore, QtGui
 
 from color_triangle import QtColorTriangle
@@ -26,6 +27,208 @@ QSlider::handle:horizontal:hover {
     border: 1px solid #444;ff
     border-radius: 4px;
 }"""
+
+
+class AlphaInputs(QtWidgets.QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+
+class RGBInputs(QtWidgets.QWidget):
+    value_changed = QtCore.pyqtSignal()
+
+    def __init__(self, color, parent=None):
+        super().__init__(parent)
+
+        self.color = color
+
+        self.label = "RGB:"
+
+        input_red = QtWidgets.QSpinBox(self)
+        input_green = QtWidgets.QSpinBox(self)
+        input_blue = QtWidgets.QSpinBox(self)
+
+        input_red.setMinimum(0)
+        input_green.setMinimum(0)
+        input_blue.setMinimum(0)
+
+        input_red.setMaximum(255)
+        input_green.setMaximum(255)
+        input_blue.setMaximum(255)
+
+        layout = QtWidgets.QHBoxLayout(self)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(input_red)
+        layout.addWidget(input_green)
+        layout.addWidget(input_blue)
+
+        input_red.valueChanged.connect(self._on_red_change)
+        input_green.valueChanged.connect(self._on_green_change)
+        input_blue.valueChanged.connect(self._on_blue_change)
+
+        self.input_red = input_red
+        self.input_green = input_green
+        self.input_blue = input_blue
+
+    def _on_red_change(self, value):
+        self.color.setRed(value)
+        self._on_change()
+
+    def _on_green_change(self, value):
+        self.color.setGreen(value)
+        self._on_change()
+
+    def _on_blue_change(self, value):
+        self.color.setBlue(value)
+        self._on_change()
+
+    def _on_change(self):
+        self.value_changed.emit()
+
+    def color_changed(self):
+        self.input_red.setValue(self.color.red())
+        self.input_green.setValue(self.color.green())
+        self.input_blue.setValue(self.color.blue())
+
+
+class CMYKInputs(QtWidgets.QWidget):
+    value_changed = QtCore.pyqtSignal()
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def _on_change(self):
+        pass
+
+    def color_changed(self):
+        pass
+
+
+class HEXInputs(QtWidgets.QWidget):
+    hex_regex = re.compile("^#(([0-9a-fA-F]{2}){3}|([0-9a-fA-F]){3})$")
+    value_changed = QtCore.pyqtSignal()
+
+    def __init__(self, color, parent=None):
+        super().__init__(parent)
+        self.color = color
+
+        self.label = "HEX:"
+
+        input_field = QtWidgets.QLineEdit()
+
+        layout = QtWidgets.QHBoxLayout(self)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(input_field)
+
+        input_field.textChanged.connect(self._on_change)
+
+        self.input_field = input_field
+
+    def _on_change(self):
+        input_value = self.input_field.text()
+        # TODO what if does not match?
+        if self.hex_regex.match(input_value):
+            self.color.setNamedColor(input_value)
+            self.value_changed.emit()
+
+    def color_changed(self):
+        input_value = self.input_field.text()
+        if self.hex_regex.match(input_value):
+            _cur_color = QtGui.QColor()
+            _cur_color.setNamedColor(input_value)
+            if (
+                _cur_color.red() == self.color.red()
+                and _cur_color.green() == self.color.green()
+                and _cur_color.blue() == self.color.blue()
+            ):
+                return
+        self.input_field.setText(self.color.name())
+
+
+class HSVInputs(QtWidgets.QWidget):
+    value_changed = QtCore.pyqtSignal()
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def _on_change(self):
+        pass
+
+    def color_changed(self):
+        pass
+
+class HSLInputs(QtWidgets.QWidget):
+    value_changed = QtCore.pyqtSignal()
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+    def _on_change(self):
+        pass
+
+    def color_changed(self):
+        pass
+
+
+class ColorInputsWidget(QtWidgets.QWidget):
+    color_changed = QtCore.pyqtSignal(QtGui.QColor)
+
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        color = QtGui.QColor()
+
+        hex_input = HEXInputs(color, self)
+        rgb_input = RGBInputs(color, self)
+
+        input_fields = [
+            hex_input,
+            rgb_input
+        ]
+
+        inputs_widget = QtWidgets.QWidget(self)
+        inputs_layout = QtWidgets.QGridLayout(inputs_widget)
+        inputs_layout.setContentsMargins(0, 0, 0, 0)
+
+        for row, input_field in enumerate(input_fields):
+            label_widget = QtWidgets.QLabel(input_field.label)
+            inputs_layout.addWidget(label_widget, row, 0)
+            inputs_layout.addWidget(input_field, row, 1)
+
+        layout = QtWidgets.QVBoxLayout(self)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(inputs_widget, 0)
+        spacer = QtWidgets.QWidget(self)
+        layout.addWidget(spacer, 1)
+
+        hex_input.value_changed.connect(self._on_value_change)
+        rgb_input.value_changed.connect(self._on_value_change)
+
+        self.input_fields = input_fields
+
+        self.hex_input = hex_input
+        self.rgb_input = rgb_input
+
+        self.color = color
+
+    def set_color(self, color):
+        if (
+            color.red() == self.color.red()
+            and color.green() == self.color.green()
+            and color.blue() == self.color.blue()
+        ):
+            return
+        self.color.setRed(color.red())
+        self.color.setGreen(color.green())
+        self.color.setBlue(color.blue())
+        self._on_value_change()
+
+    def _on_value_change(self):
+        for input_field in self.input_fields:
+            input_field.color_changed()
+        self.color_changed.emit(self.color)
+
+
 class ColorPickerWidget(QtWidgets.QWidget):
     color_changed = QtCore.pyqtSignal(QtGui.QColor)
 
@@ -60,16 +263,21 @@ class ColorPickerWidget(QtWidgets.QWidget):
         utils_layout.addWidget(opacity_slider)
         utils_layout.addWidget(btn_pick_color)
 
+        color_inputs = ColorInputsWidget(self)
+
         layout = QtWidgets.QHBoxLayout(self)
-        layout.addWidget(color_triangle, 1)
-        layout.addWidget(utils_widget, 0)
+        layout.addWidget(color_triangle)
+        layout.addWidget(utils_widget)
+        layout.addWidget(color_inputs)
 
         color_view.setColor(color_triangle.cur_color)
+        color_inputs.set_color(color_triangle.cur_color)
 
-        color_triangle.colorChanged.connect(self.color_changed)
-        btn_pick_color.released.connect(self.pick_color)
+        color_triangle.colorChanged.connect(self.triangle_color_changed)
         pick_widget.colorSelected.connect(self.color_changed)
+        color_inputs.color_changed.connect(self.color_changed)
         opacity_slider.valueChanged.connect(self.opacity_changed)
+        btn_pick_color.released.connect(self.pick_color)
 
         self.pick_widget = pick_widget
 
@@ -77,6 +285,7 @@ class ColorPickerWidget(QtWidgets.QWidget):
         self.color_view = color_view
         self.opacity_slider = opacity_slider
         self.btn_pick_color = btn_pick_color
+        self.color_inputs = color_inputs
 
     def setColor(self, color):
         self.opacity_changed(color.alpha())
@@ -85,9 +294,14 @@ class ColorPickerWidget(QtWidgets.QWidget):
     def pick_color(self):
         self.pick_widget.screen()
 
+    def triangle_color_changed(self, color):
+        self.color_view.setColor(color)
+        self.color_inputs.set_color(color)
+
     def color_changed(self, color):
         self.color_view.setColor(color)
         self.color_triangle.setColor(color)
+        self.color_inputs.set_color(color)
 
     def opacity_changed(self, alpha):
         self.color_view.setAlpha(alpha)
